e6e4632c33c50f26854f702a4162f87d9097c1d2
AlexisQuiceno <alexisquicenorodriguez@gmail.com>
2022-08-04 00:10:58 -0500

    configure frameworks scss framework

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <section>
+    <header>
+
+    </header>
+    <nav>
+
+    </nav>
+
+    <section>
+      <figure>
+
+      </figure>
+      <article>
+
+      </article>
+    </section>
+
+    <section class="corsuel">
+
+    </section>
+    <section>
+      <figure>
+        
+      </figure>
+      <article>
+
+      </article>
+    </section>
+  </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
 </script>
+
+<style >
+@import '../assets/styles/main.scss';
+</style>

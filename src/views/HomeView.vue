6c8b3bc7297e373c15551c857a0092513bb0c4ab
AlexisQuiceno <alexisquicenorodriguez@gmail.com>
2022-08-10 17:36:59 -0500

    simple portfolio layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,38 @@
 <template>
+  <header class="custom--header">
+    <p>YONIER ALEXIS QUICENO RODRIGUEZ</p>
+  </header>
   <section class="porfolio">
-    <header class="custom--header">
-      <p>YONIER ALEXIS QUICENO RODRIGUEZ</p>
-      <nav>
-      </nav>
-    </header>
-    <section>
-      <figure>
-      </figure>
-      <article>
-      </article>
+    <section class="person">
+      <figure class="photo"></figure>
+      <article class="tex--primari"></article>
     </section>
-    <section>
+    <section class="proyects">
+      <section class="proyects--uno">
+        <article class="tex--proyect--une"></article>
+        <figure class="img--proyect--one"></figure>
+      </section>
     </section>
-    <section>
-      <figure>
-      </figure>
-      <article>
-      </article>
+    <section class="skills">
+      <figure class="skills--one"></figure>
+      <figure class="skills--two"></figure>
+      <figure class="skills--tree"></figure>
+      <figure class="skills--four"></figure>
+
     </section>
+    <footer>
+      <section class="social--networks">
+        <figure class="social networks--une"></figure>
+        <figure class="social networks--two"></figure>
+        <figure class="social networks--tree"></figure>
+        <figure class="social--networks-four"></figure>
+      </section>
+      <section class="contact--form">
+      </section>
+    </footer>
   </section>
 </template>
-<script lang="ts">
-</script>
+<script lang="ts"></script>
 <style lang="scss">
-@import '../assets/styles/main.scss';
+@import "../assets/styles/main.scss";
 </style>
